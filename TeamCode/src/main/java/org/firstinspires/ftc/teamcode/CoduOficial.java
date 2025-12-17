a00addbebd3473a4302b74d2ba6c67399ef28bc6
Dani <bladevtm@gmail.com>
2025-12-17 11:36:12 +0200

    Base almost done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CoduOficial.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CoduOficial.java
@@ -8,12 +8,8 @@ import com.qualcomm.robotcore.util.Range;
 public class CoduOficial extends RobotHardware {
     private ElapsedTime runtime = new ElapsedTime();
 
-    double leftPower;
-    double rightPower;
-    boolean shooterToggle = false;
-    boolean prevButon = false;
-    double shooterPower = 0;
-    boolean y_press = false;
+   // double leftPower;
+   // double rightPower;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -22,111 +18,23 @@ public class CoduOficial extends RobotHardware {
         waitForStart();
 
         while (opModeIsActive()) {
-            double drive = -gamepad1.left_stick_y;
-            double turn = gamepad1.right_stick_x;
-            leftPower = Range.clip(drive + turn, -1.0, 1.0);
-            rightPower = Range.clip(drive - turn, -1.0, 1.0);
-
-            motorFs.setPower(leftPower);
-            motorSd.setPower(rightPower);
-            motorSs.setPower(leftPower);
-            motorFd.setPower(rightPower);
+          //  double drive = -gamepad1.left_stick_y;
+          //  double turn = gamepad1.right_stick_x;
+          //  leftPower = Range.clip(drive + turn, -1.0, 1.0);
+          //  rightPower = Range.clip(drive - turn, -1.0, 1.0);
+          //  motorFs.setPower(leftPower);
+          //  motorSd.setPower(rightPower);
+          //  motorSs.setPower(leftPower);
+          //  motorFd.setPower(rightPower);
             telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+           // telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
 
             intake();
             wheelMovement();
-            frana();
-            cureatake();
+            lanttake();
             Launch();
         }
     }
-
-    private void wheelMovement() {
-        float x, y, t;
-        t = -gamepad1.right_stick_x;
-        y = gamepad1.right_trigger - gamepad1.left_trigger;
-        x = gamepad1.left_stick_x;
-
-        float fd, fs, sd, ss;
-        fs = y + x - t;
-        fd = y - x + t;
-        ss = y - x - t;
-        sd = y + x + t;
-
-        motorFs.setPower(fs);
-        motorFd.setPower(fd);
-        motorSs.setPower(ss);
-        motorSd.setPower(sd);
-    }
-
-   // public void setIntakeServoPosition(float pos) {
-   //     intakeServo.setPosition(pos);
-   // }
-    public void cureatake(){
-        cureaMT.setPower(-1);
-    }
-
-    //----------------INTAKEEEEE------------
-    private void intake() {
-        //  boolean intakeToggle = false;
-        //  boolean intakeButonPrev = false;
-
-        //____________in while__________________________________
-        //  boolean intake_buton = gamepad2.a;
-        //  boolean outtake_buton = gamepad2.b;
-
-        //  if (intake_buton && !intakeButonPrev) //toggle
-        //      intakeToggle = !intakeToggle; //trebuie schimbat nu sunt sigur (l-am rezolvat) (nu l-ai rezolvat...)z(sa imi iei bilele)
-        //  intakeButonPrev = intake_buton;
-
-        //  if (outtake_buton)
-        //      intakeMotor.setPower(-1.0);
-        //  else {
-        //      if (intakeToggle)
-        //          intakeMotor.setPower(1.0);
-        //      else
-        //         intakeMotor.setPower(0);
-   // }
-        if (gamepad2.xWasPressed()) {
-            intakeMotor.setPower(-1);
-            intakeServo.setPosition(0);
-        }
-
-        if(gamepad2.xWasReleased()) {
-            intakeMotor.setPower(0);
-            intakeServo.setPosition(0.5);//0 - reverse, 0.5 - stop, 1 - fata
-        }
-
-         if (gamepad2.a){
-            bila.setPosition(0);
-         }
-         else {
-             bila.setPosition(0.5);
-         }
-    }
-    //---------------------Launch-------------------------------
-    private void Launch()
-    {
-        if (gamepad2.dpad_up) //creste puterea
-            shooterPower = 1;
-        if (gamepad2.dpad_down) //scade puterea
-            shooterPower = -1;
-        if (gamepad2.dpad_left)
-            shooterPower = 0.5;
-        if (gamepad2.dpad_right)
-            shooterPower = 0.25;
-//        shooterPower = Math.max(0,Math.min(0.35  , shooterPower));//creste puterea de la 0 la puterea pusa
-        if (gamepad2.y) {
-            shooterStanga.setPower(-1);
-            shooterDreapta.setPower(1);
-        } else {
-            shooterStanga.setPower(0);
-            shooterDreapta.setPower(0);
-        }
-    }
-  // public static float intakeservo_stop = 0f;
-  // public static int intakeservo_run = -1;
 }
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -6,15 +6,22 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
-//Pentru init, casi e gay
+
 public class RobotHardware extends LinearOpMode {
     public DcMotor motorFs, motorFd, motorSs, motorSd;
     public DcMotor[] wheelMotors = new DcMotor[4];
     public DcMotor intakeMotor;
     public DcMotor shooterStanga, shooterDreapta;
     public Servo intakeServo;
-    public DcMotor cureaMT;
-    public Servo bila;//casi e gay
+    public DcMotor lantMT;
+    public Servo bila;
+    double shooterPower = 0;//casi e gay
+    boolean toggleB = false;
+    boolean toggleY = false;
+    boolean toggleX = false;
+    boolean prev_pressedX = false;
+    boolean prev_pressedY = false;
+    boolean prev_pressedB = false;
     Servo odoWheelY, odoWheelX;
 
     void init_hardware(HardwareMap ahwMap) {
@@ -26,7 +33,7 @@ public class RobotHardware extends LinearOpMode {
         shooterStanga = ahwMap.get(DcMotor.class, "shooter stanga");
         shooterDreapta = ahwMap.get(DcMotor.class, "shooter dreapta");
         intakeServo = ahwMap.get(Servo.class, "servo intake");
-        cureaMT = ahwMap.get(DcMotor.class,"curea mt");
+        lantMT = ahwMap.get(DcMotor.class,"lant mt");
         bila = ahwMap.get(Servo.class,"bila");
 
         odoWheelX = ahwMap.get(Servo.class, "odometry X");
@@ -50,8 +57,6 @@ public class RobotHardware extends LinearOpMode {
 
         wheelMotors = new DcMotor[]{motorFs, motorFd, motorSs, motorSd};
 
-        frana();
-
         for (DcMotor motor : wheelMotors)
             motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
@@ -66,6 +71,106 @@ public class RobotHardware extends LinearOpMode {
     public void setOdoPositions(float position) {
         odoWheelX.setPosition(position);
         odoWheelY.setPosition(position + 0.01f);
+    }
+
+    //----------------INTAKEEEEE------------
+
+    public void intake() {
+
+
+        if (!prev_pressedX && gamepad2.x) {
+            toggleX = !toggleX;
+        }
+
+        if (toggleX) {
+
+            intakeMotor.setPower(-1);
+            intakeServo.setPosition(0);
+
+        } else {
+
+            intakeMotor.setPower(0);
+            intakeServo.setPosition(0.5);
+        }
+
+        prev_pressedX = gamepad2.x;
+
+        if (gamepad2.a){
+            bila.setPosition(0);
+        }
+        else {
+            bila.setPosition(0.5);
+        }
+    }
+
+    //---------------------Launch-------------------------------
+    public void Launch()
+    {
+        if (gamepad2.dpad_up) //creste puterea
+            shooterPower = 1;
+
+        if (gamepad2.dpad_down) //scade puterea
+            shooterPower = -1;
+
+        if (gamepad2.dpad_left)
+            shooterPower = 0.5;
+
+        if (gamepad2.dpad_right)
+            shooterPower = 0.25;
+
+        if (!prev_pressedY && gamepad2.y) {
+            toggleY = !toggleY;
+        }
+
+        if (toggleY) {
+            shooterStanga.setPower(-1);
+            shooterDreapta.setPower(1);
+        } else {
+            shooterStanga.setPower(0);
+            shooterDreapta.setPower(0);
+        }
+
+        prev_pressedY = gamepad2.y;
+    }
+
+    //------------LANT-------------
+    public void lanttake(){
+
+        if (!prev_pressedB && gamepad2.b) {
+            toggleB = !toggleB;
+        }
+
+        if (toggleB) {
+            lantMT.setPower(-1);
+        } else {
+            lantMT.setPower(0);
+        }
+
+        prev_pressedB = gamepad2.b;
+    }
+
+    //----------------WHEELS----------------
+    public void wheelMovement() {
+        if (gamepad1.a) {
+            frana();
+            return;
+        }
+
+        float x, y, t;
+        t = -gamepad1.right_stick_x;
+        y = gamepad1.right_trigger - gamepad1.left_trigger;
+        x = gamepad1.left_stick_x;
+
+        float fd, fs, sd, ss;
+        fs = y + x - t;
+        fd = y - x + t;
+        ss = y - x - t;
+        sd = y + x + t;
+
+        motorFs.setPower(fs);
+        motorFd.setPower(fd);
+        motorSs.setPower(ss);
+        motorSd.setPower(sd);
     }
 
     @Override//trb ca sa nu dea eroare
